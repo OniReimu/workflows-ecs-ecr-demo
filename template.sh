1aaab1939740886fff4bc36c4a1b5c3bd4609d60
OniReimu <reimu-saber@hotmail.com>
2016-12-08 22:07:33 +1100

    change url

--- template.sh
+++ template.sh
@@ -17,7 +17,7 @@ cat > workflows-demo-task-definition.json <<EOF
       "name": "todo-demo",
       "environment": [],
       "links": [],
-      "image": "$AWS_REGISTRY_URL/workflows-demo:$WERCKER_BRANCH_NAME",
+      "image": "854586034747.dkr.ecr.us-west-2.amazonaws.com/workflows-demo:$WERCKER_BRANCH_NAME",
       "command": [],
       "cpu": 10
     }
